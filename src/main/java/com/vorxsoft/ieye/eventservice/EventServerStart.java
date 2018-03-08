d536defc8bb08a6aff230cd20352dbdbc53f1c4e
boundlesswu <boundlesswu@gmail.com>
2018-03-08 15:01:34 +0800

    123

--- src/main/java/com/vorxsoft/ieye/eventservice/EventServerStart.java
+++ src/main/java/com/vorxsoft/ieye/eventservice/EventServerStart.java
@@ -10,6 +10,7 @@ import com.vorxsoft.ieye.eventservice.grpc.LogServiceClient;
 import com.vorxsoft.ieye.eventservice.grpc.VsIAClient;
 import com.vorxsoft.ieye.eventservice.grpc.VsIeyeClient;
 import com.vorxsoft.ieye.eventservice.process.AlarmProcess;
+import com.vorxsoft.ieye.eventservice.util.ConfigReadUtils;
 import com.vorxsoft.ieye.eventservice.util.IaagMap;
 import com.vorxsoft.ieye.eventservice.util.IaagMapItem;
 import com.vorxsoft.ieye.eventservice.util.RedisUtil;
@@ -21,15 +22,7 @@ import io.grpc.Server;
 import io.grpc.netty.NettyServerBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.dom4j.Attribute;
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.Element;
-import org.dom4j.io.SAXReader;
-import redis.clients.jedis.Jedis;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.sql.*;
@@ -395,108 +388,27 @@ public class EventServerStart implements WatchCallerInterface {
     setXXXClient(CmsType, address);
   }
 
-  public void getConfigPath() throws FileNotFoundException {
-    String tmp = String.valueOf(this.getClass().getClassLoader().getResource(cfgFileName));
-    System.out.println("tmp:" + tmp);
-    if (tmp.startsWith("jar"))
-      cfgFile = new FileInputStream(new File(System.getProperty("user.dir") + File.separator + cfgFileName));
-    else
-      cfgFile = this.getClass().getClassLoader().getResourceAsStream(cfgFileName);
-  }
 
   public void cfgInit() throws FileNotFoundException {
-    // 解析books.xml文件
-    // 创建SAXReader的对象reader
-    getConfigPath();
-    SAXReader reader = new SAXReader();
-    try {
-      System.out.println("cfg file is:" + cfgFile);
-      // 通过reader对象的read方法加载books.xml文件,获取docuemnt对象。
-      //Document document = reader.read(new File(cfgFile));
-      Document document = reader.read(cfgFile);
-      // 通过document对象获取根节点bookstore
-      Element bookStore = document.getRootElement();
-      // 通过element对象的elementIterator方法获取迭代器
-      Iterator it = bookStore.elementIterator();
-      // 遍历迭代器，获取根节点中的信息（书籍）
-      while (it.hasNext()) {
-        //System.out.println("=====开始遍历某一本书=====");
-        Element cfg = (Element) it.next();
-        // 获取book的属性名以及 属性值
-        List<Attribute> bookAttrs = cfg.attributes();
-        System.out.println("cfgname :" + cfg.getName());
-        for (Attribute attr : bookAttrs) {
-          //System.out.println("属性名：" + attr.getName() + "--属性值：" + attr.getValue());
-        }
-        String tname = cfg.getName();
-        //解析子节点的信息
-        Iterator itt = cfg.elementIterator();
-        while (itt.hasNext()) {
-          Element bookChild = (Element) itt.next();
-          String lname = bookChild.getName();
-          String lvalue = bookChild.getStringValue();
-          //System.out.println("节点名：" + bookChild.getName() + "--节点值：" + bookChild.getStringValue());
-          if (tname.equals("info")) {
-            if (lname.equals("hostip"))
-              hostip = lvalue;
-            else if (lname.equals("port"))
-              PORT = Integer.parseInt(lvalue);
-            else if (lname.equals("name"))
-              serviceName = lvalue;
-            else if (lname.equals("ttl"))
-              ttl = Integer.parseInt(lvalue);
-          }
-          if (tname.equals("database")) {
-            if (lname.equals("name"))
-              dbname = lvalue;
-            else if (lname.equals("address"))
-              dbAddress = lvalue;
-            else if (lname.equals("user"))
-              dbUser = lvalue;
-            else if (lname.equals("passwd"))
-              dbPasswd = lvalue;
-            else if (lname.equals("driverClassName"))
-              driverClassName = lvalue;
-          }
-          if (tname.equals("registerCenter")) {
-            if (lname.equals("name"))
-              registerCenterName = lvalue;
-            else if (lname.equals("address"))
-              registerCenterAddress = lvalue;
-          }
-          if (tname.equals("redis")) {
-            if (lname.equals("name"))
-              redisName = lvalue;
-            else if (lname.equals("ip"))
-              redisIP = lvalue;
-            else if (lname.equals("port"))
-              redisPort = Integer.parseInt(lvalue);
-          }
-          if (tname.equals("activemq")) {
-            if (lname.equals("name"))
-              activemqName = lvalue;
-            else if (lname.equals("ip"))
-              activemqIp = lvalue;
-            else if (lname.equals("port"))
-              activemqPort = Integer.parseInt(lvalue);
-          }
-          if (tname.equals("alarmBell")) {
-            if (lname.equals("name")) {
-            } else if (lname.equals("ip")) {
-              alarmBellIp = lvalue;
-            } else if (lname.equals("port")) {
-              alarmBellPort = Integer.parseInt(lvalue);
-            } else if (lname.equals("url")) {
-              alarmBellUrl = lvalue;
-            }
-          }
-        }
-        //System.out.println("=====结束遍历某一本书=====");
-      }
-    } catch (DocumentException e) {
-      e.printStackTrace();
-      getLogger().error(e.getMessage(), e);
-    }
+    ConfigReadUtils configReadUtils = new ConfigReadUtils();
+    configReadUtils.cfgInit();
+    hostip = configReadUtils.getHostip();
+    PORT = configReadUtils.getEmsPort();
+    ttl = configReadUtils.getTtl();
+    dbname = configReadUtils.getDbname();
+    dbUser = configReadUtils.getDbUser();
+    dbPasswd = configReadUtils.getDbPasswd();
+    driverClassName = configReadUtils.getDriverClassName();
+    dbAddress = configReadUtils.getDbAddress();
+    redisName = configReadUtils.getRedisName();
+    redisIP = configReadUtils.getRedisIP();
+    redisPort = configReadUtils.getRedisPort();
+    activemqName = configReadUtils.getActivemqName();
+    activemqIp = configReadUtils.getActivemqIp();
+    activemqPort = configReadUtils.getActivemqPort();
+    alarmBellIp = configReadUtils.getAlarmBellIp();
+    activemqPort = configReadUtils.getActivemqPort();
+    alarmBellUrl = configReadUtils.getAlarmBellUrl();
   }
 
   public List<ReloadRequest> getReloadRequest() throws JsonFormat.ParseException {
